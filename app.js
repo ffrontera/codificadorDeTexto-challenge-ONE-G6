75b2b163ae3be92a59679ac39bf29ebf35e3fd82
Federico Frontera <ffrontera@live.com>
2024-02-08 17:59:30 -0300

    se añadio la funcion verificarTexto() y se conecto con el textarea, se agrego un enlace para refrescar la pagina en el logo

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const salidaTextoAntes = document.getElementById("antes");
 const parrafoSalidaTexto = document.getElementById("main-salidaTexto");
 const botonEncriptar = document.getElementById('encriptar');
 const botonDesencriptar = document.getElementById('desencriptar');
+const textoAdvertencia = document.getElementById('advertencia');
 
 function encriptarTexto(texto){
     
@@ -66,7 +67,11 @@ function desencriptarTexto(texto){
 }
 
 function mostrarTexto(accion){
-    
+    if(entradaTexto.value == ''){
+        return
+    }
+
+
     salidaTextoAntes.style.display = "none";
     salidaTextoDespues.style.display = 'flex';
     
@@ -99,4 +104,16 @@ async function copiarTexto(){
     entradaTexto.disabled = false;
 
     alert('¡Mensaje copiado al portapapeles!')
+}
+const permitidos = /^[a-z0-9 ]+$/;
+function verificarTexto(texto){
+    if (permitidos.test(texto) || texto == ''){
+        botonEncriptar.disabled = false; 
+        botonDesencriptar.disabled = false;
+        textoAdvertencia.style.color = 'darkgrey';
+    }else{
+        botonEncriptar.disabled = true; 
+        botonDesencriptar.disabled = true;
+        textoAdvertencia.style.color = 'lightcoral';
+    }
 }
